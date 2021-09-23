82a8d6bcb1c2e5cad8d843154d5119d0843fe5f4
fedepujol <9yjbd325@anonaddy.me>
2021-09-23 18:14:33 -0300

    Refactor Move into MoveLine and MoveBlock

--- lua/move.lua
+++ lua/move.lua
@@ -1,19 +1,3 @@
-DOWN = -1
-UP = 1
-
--- Contains
--- tab: table of values
--- val: value to check if exists
--- return boolean
-function Contains(tab, val)
-	for _, value in ipairs(tab) do
-		if val == value then
-			return true
-		end
-	end
-	return false
-end
-
 -- Move
 -- cursor: position of cursor in the form of {line, col}
 -- start_pos: position to get the lines. Cero indexed
@@ -28,105 +12,100 @@ function Move(cursor, start_pos, end_pos, dir)
 	target_line = vim.api.nvim_buf_get_lines(0, start_pos, end_pos, true)
 
 	-- Swap current with target
-	if dir > 0 then
-		if vim.fn.mode():byte() == 86 or vim.fn.mode():byte() == 118 then
-            local vSRow = vim.fn.line('v') - 1
-			local vERow= vim.api.nvim_win_get_cursor(0)[1]
-
-			if vSRow > vERow then
-				local aux = vSRow
-				vSRow = vERow
-				vERow = aux
-			end
-
-			local vBlock = vim.api.nvim_buf_get_lines(0, vSRow, vERow, true)
-			local vTarget = vim.api.nvim_buf_get_lines(0, vSRow - 1, vSRow, true)
-
-			table.insert(vBlock, vTarget[1])
-
-			vim.api.nvim_buf_set_lines(0, vSRow - 1, vERow, true, vBlock)
-			vim.api.nvim_exec('execute "normal! \\e\\e"', false)
-			vim.api.nvim_exec(':normal! '..vSRow..'ggV'..vERow..'gg', false)
-		else
+	if dir < 0 then
+		vim.api.nvim_buf_set_lines(0, start_pos, end_pos, true, { current_line })
+		vim.api.nvim_buf_set_lines(0, end_pos, cursor_line, true, target_line)
+	else if dir > 0 then
+			vim.api.nvim_buf_set_lines(0, start_pos - 1, end_pos - 1, true, target_line)
 			vim.api.nvim_buf_set_lines(0, start_pos, end_pos, true, { current_line })
-			vim.api.nvim_buf_set_lines(0, end_pos, cursor_line, true, target_line)
-		end
-		else if dir < 0 then
-			if vim.fn.mode():byte() == 86 or vim.fn.mode():byte() == 118 then
-				local vSRow = vim.fn.line('v') - 1
-				local vERow= vim.api.nvim_win_get_cursor(0)[1]
-
-				if vSRow > vERow then
-					local aux = vSRow
-					vSRow = vERow
-					vERow = aux
-				end
-
-				local vBlock = vim.api.nvim_buf_get_lines(0, vSRow, vERow, true)
-				local vTarget = vim.api.nvim_buf_get_lines(0, vERow, vERow + 1, true)
-
-				table.insert(vBlock, 1, vTarget[1])
-				vim.api.nvim_buf_set_lines(0, vSRow, vSRow + (vERow - vSRow) + 1, true, vBlock)
-				vim.api.nvim_exec('execute "normal! \\e\\e"', false)
-				vim.api.nvim_exec(':normal! '..(vSRow + 2)..'ggV'..(vERow + 1)..'gg', false)
-			else
-				vim.api.nvim_buf_set_lines(0, start_pos - 1, end_pos - 1, true, target_line)
-				vim.api.nvim_buf_set_lines(0, start_pos, end_pos, true, { current_line })
-			end
 		end
 	end
+
 	-- Set cursor position
 	vim.api.nvim_win_set_cursor(0, { end_pos, cursor[2] })
 end
 
-function MoveLine(direction)
+function MoveLine(dir)
 	-- Get the last line of current buffer
 	local last_line = vim.fn.line('$')
 
 	-- Get current cursor position
 	local cursor_position = vim.api.nvim_win_get_cursor(0)
 	local line = cursor_position[1]
-	local valid_directions = {DOWN, UP}
-	local res = nil
 
-	if direction == nil then
-		vim.api.nvim_notify('Missing direction', 4, {})
-		return
-	end
-
-	res = Contains(valid_directions, direction)
-
-	if res == nil or not res then
-		vim.api.nvim_notify('Invalid direction', 4, {})
+	if dir == nil then
+		vim.api.nvim_notify('Missing offset', 4, {})
 		return
 	end
 
 	-- Edge Case
-	if line == 1 and direction < 0 then
-		Move(cursor_position, line, line + 1, DOWN)
+	if line == 1 and dir > 0 then
+		Move(cursor_position, line, line + 1, dir)
 		return
 	end
-	if line == last_line and direction > 0 then
-		Move(cursor_position, line - 2, line -1, UP)
+	if line == last_line and dir < 0 then
+		Move(cursor_position, line - 2, line -1, dir)
 		return
 	end
 
 	-- General Case
 	if line > 1 and line < last_line then
-		if direction < 0 then
-			Move(cursor_position, line, line + 1, DOWN)
+		if dir > 0 then
+			Move(cursor_position, line, line + 1, dir)
 			return
 		end
-		if direction > 0 then
-			Move(cursor_position, line - 2, line - 1, UP)
+		if dir < 0 then
+			Move(cursor_position, line - 2, line - 1, dir)
 			return
 		end
 	end
 end
 
-vim.api.nvim_set_keymap('n', '<C-A-k>', "<Cmd>lua MoveLine(1)<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-A-j>', "<Cmd>lua MoveLine(-1)<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', '<C-A-k>', "<Cmd>lua MoveLine(1)<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', '<C-A-j>', "<Cmd>lua MoveLine(-1)<CR>", { noremap = true, silent = true })
+local function get_target(sRow, eRow)
+	return vim.api.nvim_buf_get_lines(0, sRow, eRow, true)
+end
 
-return { Move = MoveLine }
+local function move_block(block, sRow, eRow)
+	vim.api.nvim_buf_set_lines(0, sRow, eRow, true, block)
+	vim.api.nvim_exec('execute "normal! \\e\\e"', false)
+end
+
+function MoveBlock(dir, line1, line2)
+	local vSRow = line1 or vim.fn.line('v')
+	local vERow = line2 or vim.api.nvim_win_get_cursor(0)[1]
+
+	-- Zero-based and end exclusive
+	vSRow = vSRow - 1
+
+	if vSRow > vERow then
+		local aux = vSRow
+		vSRow = vERow
+		vERow = aux
+	end
+
+	local vBlock = vim.api.nvim_buf_get_lines(0, vSRow, vERow, true)
+	if dir < 0 then
+		local vTarget = get_target(vSRow - 1, vSRow)
+
+		table.insert(vBlock, vTarget[1])
+
+		move_block(vBlock, vSRow - 1, vERow)
+		vim.api.nvim_exec(':normal! '..vSRow..'ggV'..(vERow - 1)..'gg', false)
+	else
+		if dir > 0 then
+			local vTarget = get_target(vERow, vERow + 1)
+
+			table.insert(vBlock, 1, vTarget[1])
+
+			move_block(vBlock, vSRow, vERow + 1)
+			vim.api.nvim_exec(':normal! '..(vSRow + 2)..'ggV'..(vERow + 1)..'gg', false)
+		end
+	end
+end
+
+vim.api.nvim_set_keymap('n', '<C-A-j>', "<Cmd>lua MoveLine(1)<CR>", { noremap = true, silent = true, nowait = true })
+vim.api.nvim_set_keymap('n', '<C-A-k>', "<Cmd>lua MoveLine(-1)<CR>", { noremap = true, silent = true, nowait = true })
+vim.api.nvim_set_keymap('v', '<C-A-k>', "<Cmd>lua MoveBlock(-1)<CR>", { noremap = true, silent = true, nowait = true })
+vim.api.nvim_set_keymap('v', '<C-A-j>', "<Cmd>lua MoveBlock(1)<CR>", { noremap = true, silent = true, nowait = true })
+
+return { MoveLine = MoveLine, MoveBlock = MoveBlock }
